a1a8a778a5e9b624fcb0939a8dda5ac439f10548
Alison Thomson <alisonmcknight@hotmail.com>
2024-01-18 15:02:43 +0000

    Add list items and form

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
-
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', items=get_items())
+
+@app.post('/add-item')
+def add_item_request():
+    new_item = request.form.get('title')
+    add_item(new_item)
+    return redirect(url_for('index'))
