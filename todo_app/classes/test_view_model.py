f26c8830f06e9c44f0e9c7eb053e179953ff969f
Alison Thomson <alisonmcknight@hotmail.com>
2024-03-15 15:15:24 +0000

    Module 3: Write failing test

---
+++ todo_app/classes/test_view_model.py
@@ -0,0 +1,19 @@
+from todo_app.classes.card import Card
+from todo_app.classes.view_model import ViewModel
+
+
+def test_view_model_done_property():
+# Arrange
+    done_cards = []
+    done_cards.append(Card(1, "test1", "Done"))
+    done_cards.append(Card(2, "test2", "Done"))
+    done_cards.append(Card(3, "test3", "Done"))
+
+    done_items_view_model = ViewModel(done_cards)
+
+# Act
+    result = done_items_view_model.done_items
+
+# Assert
+    assert len(result) == 3
+    assert result[0].name == "test1"
