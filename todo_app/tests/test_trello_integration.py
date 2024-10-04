6a35c7163615af35db5fb368337bc91b60b73479
Alison Thomson <Alison.Thomson@softwire.com>
2024-10-04 15:45:02 +0100

    Module 10: Update tests

--- todo_app/tests/test_trello_integration.py
+++ todo_app/tests/test_trello_integration.py
@@ -1,49 +1,35 @@
 import pytest
+import mongomock
 from dotenv import find_dotenv, load_dotenv
-import requests
-
 
 from todo_app import app
-from todo_app.classes.trello_service import TrelloService
-from todo_app.data.trello_items import get_cards
-
-
+from todo_app.classes.mongodb_service import MongoDbService
 
 @pytest.fixture
 def client():
-    file_path = find_dotenv('.env.test.')
+    file_path = find_dotenv('.env.test')
     load_dotenv(file_path, override=True)
-
-    test_app = app.create_app()
-
-    with test_app.test_client() as client:
-        yield client
-
-class StubResponse():
-    def __init__(self, fake_response_data):
-        self.fake_response_data = fake_response_data
-
-    def json(self):
-        return self.fake_response_data
     
-def stub(url, params={}, headers={}):
-    testTrello = TrelloService()
-    test_board_id = testTrello.board_id
+    with mongomock.patch(servers=(('fakemongo.com', 27017),)):
+        test_app = app.create_app()
+        with test_app.test_client() as client:
+            yield client
 
-    if url == f'https://api.trello.com/1/boards/{test_board_id}/lists?cards=open&card_fields=id,name&fields=name':
-        fake_response_data = [{
-            'id': '1',
-            'name': 'To Do',
-            'cards': [{'id': '456', 'name': 'Test card'}]
-        }]
-        return StubResponse(fake_response_data)
+def test_index_page(client):
+    # Arrange
+    mongoDbService = MongoDbService()
 
-    raise Exception(f'Integration test did not expect URL "{url}"')
+    test_document = {
+        "name": "Test item",
+        "status": "To do"
+    }
 
-def test_index_page(monkeypatch, client):
-    monkeypatch.setattr(requests, 'get', stub)
+    mongoDbService.todo_items.insert_one(test_document)
 
+    # Act
     response = client.get('/')
 
+
+    # Assert
     assert response.status_code == 200
-    assert 'Test card' in response.data.decode()
+    assert "Test item" in response.data.decode()
