6840d7243fec3dbcaadbf377e1a5b9817cd4f959
Alison Thomson <Alison.Thomson@softwire.com>
2024-10-15 17:31:15 +0100

    Module 10: Markup

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,22 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+from todo_app.classes.item_service import ItemService
 from todo_app.classes.view_model import ViewModel
 from todo_app.flask_config import Config
-from todo_app.data.mongo_items import get_items, add_item, update_item
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
+    item_service = ItemService()
 
     @app.route('/')
     def index():
-        items_view_model = ViewModel(get_items())
+        items_view_model = ViewModel(item_service.get_items())
         return render_template('index.html', view_model=items_view_model)
 
     @app.post('/add-item')
     def add_item_request():
         new_item = request.form.get('title')
-        add_item(new_item)
+        item_service.add_item(new_item)
         return redirect(url_for('index'))
 
     @app.post('/complete-item')
@@ -24,7 +25,7 @@ def create_app():
         item_id = request.form.get('item_id')
         print(item_id)
         
-        update_item(item_id, "Done")
+        item_service.update_item(item_id, "Done")
         return redirect(url_for('index'))
     
     return app
