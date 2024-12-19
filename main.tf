9205f311b9777a8bd79dd3cd76f0254c0ec892f0
Alison Thomson <Alison.Thomson@softwire.com>
2024-12-19 14:15:37 +0000

    Module 12: Create tf app service and web app

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 3.8"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "d33b95c7-af3c-4247-9661-aa96d47fccc0"
+}
+
+data "azurerm_resource_group" "main" {
+  name     = "cohort32-33_AliTho_ProjectExercise"
+}
+
+resource "azurerm_service_plan" "main" {
+  name                = "terraformed-asp"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "main" {
+  name                = "alithotfapp"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  service_plan_id     = azurerm_service_plan.main.id
+
+  site_config {
+    application_stack {
+      docker_image_name     = "appsvcsample/python-helloworld:latest"
+      docker_registry_url   = "https://index.docker.io"
+    }
+  }
+}
