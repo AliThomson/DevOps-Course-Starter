01e6d2ee0995343279a148de7972cd6bab29fdae
Alison Thomson <Alison.Thomson@softwire.com>
2025-01-30 13:51:31 +0000

    Add Loggly

--- todo_app/app.py
+++ todo_app/app.py
@@ -4,6 +4,9 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 
 from todo_app.oauth import login_required, blueprint
 
+from loggly.handlers import HTTPSHandler
+from logging import Formatter
+
 from todo_app.classes.item_service import ItemService
 from todo_app.classes.view_model import ViewModel
 from todo_app.flask_config import Config
@@ -12,7 +15,14 @@ def create_app():
     app = Flask(__name__)
     app.wsgi_app = ProxyFix(app.wsgi_app)
     app.config.from_object(Config())
+
     app.logger.setLevel(os.environ.get('LOG_LEVEL'))
+    if app.config['LOGGLY_TOKEN'] is not None:
+        handler = HTTPSHandler(f'https://logs-01.loggly.com/inputs/{app.config["LOGGLY_TOKEN"]}/tag/todo-app')
+        handler.setFormatter(
+            Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s")
+        )
+        app.logger.addHandler(handler)
 
     app.register_blueprint(blueprint, url_prefix="/login")
     
--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -5,3 +5,4 @@ class Config:
     def __init__(self):
         """Base configuration variables."""
         self.SECRET_KEY = os.environ.get('SECRET_KEY')
+        self.LOGGLY_TOKEN = os.environ.get('LOGGLY_TOKEN')
